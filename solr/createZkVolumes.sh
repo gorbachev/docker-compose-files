61da006f54b82b3ef8e3dd1a46763dc24d09328e
Tero Paananen <tpp@iki.fi>
2017-09-24 23:25:04 -0400

    Solr Cloud cluster

---
+++ solr/createZkVolumes.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p /srv/zk/solr/zk1/data
+mkdir -p /srv/zk/solr/zk1/log
+mkdir -p /srv/zk/solr/zk2/data
+mkdir -p /srv/zk/solr/zk2/log
+mkdir -p /srv/zk/solr/zk3/data
+mkdir -p /srv/zk/solr/zk3/log
---
+++ solr/resetZk.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf /srv/zk/solr/zk1/data/*
+rm -rf /srv/zk/solr/zk1/log/*
+rm -rf /srv/zk/solr/zk2/data/*
+rm -rf /srv/zk/solr/zk2/log/*
+rm -rf /srv/zk/solr/zk3/data/*
+rm -rf /srv/zk/solr/zk3/log/*
